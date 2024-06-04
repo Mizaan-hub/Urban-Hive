71914f91b73541d49bc3ebf1853333441f041045
Mizaan-hub <mizaanafandi@gmail.com>
2024-06-04 20:39:37 +0530

    db testing postman testing success

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,17 +1,36 @@
-import bcrypt from 'bcrypt'
+import bcrypt from "bcrypt";
+import prisma from "../lib/prisma.js";
 
 export const register = async (req, res) => {
     const { username, email, password } = req.body;
 
-    //Hashing password
-    const hashedPassword = await bcrypt.hash(password, 10);
-    console.log(hashedPassword);
+    try {
+        //Hashing password
+        const hashedPassword = await bcrypt.hash(password, 10);
+        console.log(hashedPassword);
+
+    // Create a new user and save to db
+    const newUser = await prisma.user.create({
+        data: {
+            username,
+            email,
+            password: hashedPassword,
+        },
+    });
+
+    console.log(newUser);
+    res.status(201).json({ message: "User Created Successfully!" });
+    } 
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Error creating user" });
+    }
 };
 
-export const login  = (req, res) => {
+export const login = (req, res) => {
 
-}
+};
 
 export const logout = (req, res) => {
-
-}
+    
+};
