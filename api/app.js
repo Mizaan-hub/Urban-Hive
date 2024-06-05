0b86b126bcdcfc8ade632846e085a3dd91e63f29
Mizaan-hub <mizaanafandi@gmail.com>
2024-06-05 19:55:38 +0530

    Added User routes

--- api/app.js
+++ api/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser"
 import authRoute from "./routes/auth.route.js";
 import testRoute from "./routes/test.route.js";
+import UserRoute from "./routes/user.route.js";
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(cookieParser());
 
 app.use('/api/auth', authRoute)
 app.use('/api/test', testRoute)
+app.use('api/user', UserRoute)
 
 app.listen(6969, () => {
     console.log("Server is running on port 6969");
---
+++ api/controllers/user.controller.js
@@ -0,0 +1,47 @@
+export const getUsers = async (req, res) => {
+  try {
+    
+  } 
+  catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Failed To Get User 😔',
+    })
+  }
+}
+
+export const getUser = async (req, res) => {
+  try {
+    
+  } 
+  catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Failed To Get User 😔',
+    })
+  }
+}
+
+export const updateUser = async (req, res) => {
+  try {
+    
+  } 
+  catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Failed To Get User 😔',
+    })
+  }
+}
+
+export const deleteUser = async (req, res) => {
+  try {
+    
+  } 
+  catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Failed To Get User 😔',
+    })
+  }
+}
