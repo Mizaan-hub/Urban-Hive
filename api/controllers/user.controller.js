98784efeb71c85eeaac5bbdd559ac89ba0781ed5
Mizaan-hub <mizaanafandi@gmail.com>
2024-06-07 23:04:50 +0530

    Profile updated

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -123,3 +123,24 @@ export const savePost = async (req, res) => {
     res.status(500).json({ message: 'Failed To Save Post 😔', });
   }
 };
+
+export const profilePosts = async (req, res) => {
+  const tokenUserId = req.userId;
+  try {
+    const userPosts = await prisma.post.findMany({
+      where: { userId: tokenUserId },
+    });
+    const saved = await prisma.savedPost.findMany({
+      where: { userId: tokenUserId },
+      include: {
+        post: true,
+      },
+    });
+
+    const savedPosts = saved.map((item) => item.post);
+    res.status(200).json({ userPosts, savedPosts });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to get profile posts!" });
+  }
+};
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,13 +1,14 @@
 import express from "express"
-import { deleteUser, getUser, getUsers, savePost, updateUser } from "../controllers/user.controller.js";
+import { deleteUser, getUser, getUsers, profilePosts, savePost, updateUser } from "../controllers/user.controller.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
 router.get("/", getUsers)
-router.get("/:id", verifyToken ,getUser)
+// router.get("/search:id", verifyToken ,getUser)
 router.put("/:id", verifyToken ,updateUser)
 router.delete("/:id", verifyToken ,deleteUser)
 router.post("/save", verifyToken ,savePost)
+router.get("/profilePosts", verifyToken, profilePosts)
 
 export default router
