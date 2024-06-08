0dd8299cdb5da6bb1b88f6440cd5e28d045e5d15
Mizaan-hub <mizaanafandi@gmail.com>
2024-06-09 03:04:20 +0530

    Initiated server side and client side socket io

---
+++ socket/app.js
@@ -0,0 +1,13 @@
+import { Server, Socket } from "socket.io";
+
+const io = new Server({
+  cors:{
+    origin:"http://localhost:6969",
+  }
+})
+
+io.on("connection", (socket) => {
+  console.log(socket);
+})
+
+io.listen("80085")
