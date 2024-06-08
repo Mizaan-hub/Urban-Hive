15746bb2362886efefe1d57115128a3b2a6082a6
Mizaan-hub <mizaanafandi@gmail.com>
2024-06-09 04:40:34 +0530

    Working on chat is done

--- socket/app.js
+++ socket/app.js
@@ -6,8 +6,39 @@ const io = new Server({
   }
 })
 
+let onlineUser = []
+
+const addUser = (userId, socketId) => {
+  const userExits = onlineUser.find((user) => user.userId === userId)
+
+  if(!userExits){
+    onlineUser.push({ userId, socketId })
+  }
+}
+
+const removeUser = (socketId) => {
+  onlineUser = onlineUser.filter(user => user.socketId !== socketId)
+}
+
+const getUser = (userId) => {
+  return onlineUser.find((user) => user.userId === userId)
+}
+
 io.on("connection", (socket) => {
-  console.log(socket);
+
+  socket.on("newUser", (userId) => {
+    addUser(userId, socket.id)
+  })
+
+
+  socket.on("sendMessage", ({receiverId, data}) => {
+    const receiver = getUser(receiverId)
+    io.to(receiver.socketId).emit("getMessage",data)
+  })
+
+  socket.on("disconnect", () =>{
+    removeUser(socket.id)
+  })
 })
 
 io.listen("8008")
